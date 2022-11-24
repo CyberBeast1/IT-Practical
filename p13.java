2e0bbe01b3af60217466363db29ad42ef10faaaf
Red-dragon-wings <cyberdart1410@gmail.com>
2022-11-24 18:51:23 +0530

    added pending programs

--- p13.java
+++ p13.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class p13{
     public static void main(String[] args){
-		int a=5,b=10;
+		Scanner scr = new Scanner(System.in);
+		int a,b;
+		System.out.print("Enter a: ");
+		a = scr.nextInt();
+		System.out.print("Enter b: ");
+        b = scr.nextInt();
 		int sum = a+b;
 		System.out.printf("Sum of %d and %d is %d\n",a,b,sum);
-
+		scr.close();
     }
 }
 
